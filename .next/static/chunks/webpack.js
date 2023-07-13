8f6002ab69450c813d574fd0202e08e3738d74b4
YasaminSoleimani <soleymani.yasaman@yahoo.com>
2023-07-13 17:45:44 +0300

    final

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "7deb67f470a31a0b"; }
+/******/ 		__webpack_require__.h = function() { return "0ac412d3f18dc0a6"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
